8d7ff2639773d8b14ccaf3b8acea96f3611bab06
santosh <styadav018@gmail.com>
2023-06-15 23:46:00 +0530

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Form from "./components/Form"
 import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
@@ -8,9 +8,12 @@ import Navbar from "./Navbar"
 import Protected from "./components/Protected"
 
 export default function App() {
+  const [count, setCount] = useState("");
+  
   return (
     <div className="text-3xl font-extrabold ">
-      Hello world!
+      Hello world! {count}
+      <button className="px-4 py-1 bg-gray-800 text-slate-100" onClick={()=>setCount(pre=>pre+1)}>+1</button>
       <Navbar/>
       <Routes>
       <Route path="/" element={<Home/>}/>
